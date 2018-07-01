a3203732e86e6767604057a70e9318aaccd9efcc
Victoria Meng <nights1ststar@protonmail.com>
2018-07-01 23:11:46 +0000

    automatically backed up by learn

--- db/migrate/20180701230957_create_songs.rb
+++ db/migrate/20180701230957_create_songs.rb
@@ -2,8 +2,8 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :name
-      t.artist_id :integer
-      t.genre_id :integer
+      t.integer :genre_id
+      t.integer :artist_id
 
       t.timestamps null: false
     end
