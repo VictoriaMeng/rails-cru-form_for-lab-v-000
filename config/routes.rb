5093fddc72527cd2f9e36945939a7a8625de1680
Victoria Meng <nights1ststar@protonmail.com>
2018-07-01 23:14:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/edit'
-
-  get 'songs/update'
-
-  get 'songs/show'
-
+  resources :songs
   resources :artists
   resources :genres
 end
